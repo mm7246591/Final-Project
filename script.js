dc0b15fe908db1efd7e1ae974d584203637b4f1d
Yan <mm7246591@yahoo.com>
2021-11-27 16:51:04 +0800

    11/27

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 (function($) {
     $(function() {
         let str = '';
-    //--------首頁開始考試按鈕    
+        //--------首頁開始考試按鈕    
         $("#sel_year, #sel_obj").change(function() {
             const year = $("#sel_year").val();
             const obj = $("#sel_obj").val();
             if (year != "選擇年份" && obj != "選擇科目") $(".button").show()
-            else $(".button").hide();
         });
-        $(".button").click(function(){
+        $(".button").click(function() {
             //const year = $("#sel_year").val();
             const obj = $("#sel_obj").val();
-            switch(obj)
-            {
+            switch (obj) {
                 case "Chinese":
                     $("#home-form").attr("action", "examPage/Chinese.html");
                     break;
@@ -26,11 +24,11 @@
                     break;
             }
         });
-    //--------這是什麼我不知道（記得填）  
+        //--------答案卡顯示  
         $(".content").hover(function() {
-            $(".content").toggleClass("toggle")
-        })
-    //--------國文考試按鈕產生（？  
+                $(".content").toggleClass("toggle")
+            })
+            //--------國文答案格
         for (let i = 1; i <= 42; i++) {
             str += `<label>
             <span>${i}</span>
@@ -41,6 +39,6 @@
             </label>`
         }
         $("#answer-form").append(str);
-    //--------新增的每一個都要寫!!! Thanks
+        //--------新增的每一個都要寫!!! Thanks
     })
 })(jQuery)
