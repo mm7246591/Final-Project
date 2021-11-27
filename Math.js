356e7ed0defe43862515f68b4d59cc0818f760f7
Hunag-joyce <joyce890926@gmail.com>
2021-11-28 00:50:06 +0800

    1128

--- Math.js
+++ Math.js
@@ -1,15 +1,4 @@
-// let xhr = new XMLHttpRequest();
-// let url = '../topic API/110Math.json'
-// xhr.open('GET', url, true);
-// xhr.onload = function() {
-//     if(this.status >= 200 && this.status < 400) {
-//         let response = JSON.parse(this.response)
-//         console.log(response)
-//     }
-// };
-// xhr.onerror = function() {
-// };
-// xhr.send();
+//----串接API
 fetch("../topic API/110Math.json")
   .then((res) => {
     const data = res.json();
