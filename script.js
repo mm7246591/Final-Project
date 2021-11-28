f21f1afaf4210aaeede58323291f1554b526eac1
Yan <mm7246591@yahoo.com>
2021-11-28 23:41:33 +0800

    11/28

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 (function($) {
     $(function() {
-        let str = '';
+        let chineseStr = '';
+        let englishStr = '';
+        let mathStr = '';
         //首頁開始考試按鈕    
         $("#sel_year, #sel_obj").change(function() {
             const year = $("#sel_year").val();
@@ -28,7 +30,7 @@
         });
         //國文答案格
         for (let i = 1; i <= 42; i++) {
-            str += `<label>
+            chineseStr += `<label>
             <span>${i}</span>
             <input type="checkbox" name="Chinese" value="A">
             <input type="checkbox" name="Chinese" value="B">
@@ -36,9 +38,42 @@
             <input type="checkbox" name="Chinese" value="D">
             </label>`
         }
-        $("#from-checkbox").append(str);
-
-
+        //英文答案格
+        for (let i = 1; i <= 56; i++) {
+            englishStr += `<label>
+            <span>${i}</span>
+            <input type="checkbox" name="English" value="A">
+            <input type="checkbox" name="English" value="B">
+            <input type="checkbox" name="English" value="C">
+            <input type="checkbox" name="English" value="D">
+            <input type="checkbox" name="English" value="E">
+            <input type="checkbox" name="English" value="F">
+            <input type="checkbox" name="English" value="G">
+            <input type="checkbox" name="English" value="H">
+            <input type="checkbox" name="English" value="I">
+            <input type="checkbox" name="English" value="J">
+            </label>`
+        }
+        for (let i = 1; i <= 20; i++) {
+            mathStr += `<label>
+            <span>${i}</span>
+            <input type="checkbox" name="Math" value="1">
+            <input type="checkbox" name="Math" value="2">
+            <input type="checkbox" name="Math" value="3">
+            <input type="checkbox" name="Math" value="4">
+            <input type="checkbox" name="Math" value="5">
+            <input type="checkbox" name="Math" value="6">
+            <input type="checkbox" name="Math" value="7">
+            <input type="checkbox" name="Math" value="8">
+            <input type="checkbox" name="Math" value="9">
+            <input type="checkbox" name="Math" value="0">
+            <input type="checkbox" name="Math" value="-">
+            <input type="checkbox" name="Math" value="+/-">
+            </label>`
+        }
+        $(".Chinese>#from-checkbox").append(chineseStr);
+        $(".English>#from-checkbox").append(englishStr);
+        $(".Math>#from-checkbox").append(mathStr);
         //--------新增的每一個都要寫!!! Thanks
     })
 })(jQuery)
